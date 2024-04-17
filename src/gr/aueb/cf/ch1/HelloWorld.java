90d9b91a621f33be9bc787c8e84ada1c57556169
Triantafyllidis Apostolos <codeapplejam@gmail.com>
2024-04-17 22:19:51 +0300

    Just a Start

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints the phrase "Hello World!"
+ * in standard output (console).
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
