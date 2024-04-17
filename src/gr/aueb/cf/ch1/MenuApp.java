673c8088875086ff5cc847c83affb5913b9c7ae9
Triantafyllidis Apostolos <codeapplejam@gmail.com>
2024-04-17 23:06:11 +0300

    Week 1

---
+++ src/gr/aueb/cf/ch1/MenuApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch1;
+
+public class MenuApp {
+    public static void main(String[] args) {
+        System.out.println("Επιλεξτε μια απο τις παρακατω επιλογες:\n");
+        System.out.println("1. Εισαγωγη");
+        System.out.println("2. Διαγραφη");
+        System.out.println("3. Ενημερωση");
+        System.out.println("4. Εξοδος\n");
+        System.out.println("Δωστε αριθμο επιλογης:");
+
+    }
+}
---
+++ src/gr/aueb/cf/ch1/SumOfTwoIntegers.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch1;
+
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+        int num1 = 19;
+        int num2 = 30;
+        int sum = num1+num2;
+        System.out.println("Το αποτελεσμα της προσθεσης ειναι ισο με "+sum);
+    }
+}
